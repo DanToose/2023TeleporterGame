15e0c0cdc5556b3bdcf0b16b5ba433380c78b939
Dan Toose <dantoose@gmail.com>
2023-04-23 09:53:54 +1000

    Refactored teleport scripts to work on single files
    No more having to set cooldowns in two places. It's now all on the character.

--- Assets/Scripts/ShortRangedTeleport.cs
+++ Assets/Scripts/ShortRangedTeleport.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShortRangedTeleport : MonoBehaviour
 {
@@ -9,40 +10,44 @@ public class ShortRangedTeleport : MonoBehaviour
     public bool shortOnCooldown;
     private float baseCooldown;
     public float shortCooldown;
+    private float cooldownTotal;
+
     public KeyCode m_short;
-    //public float shortTeleport;
     public Transform shortTeleTarget;
     public GameObject Player;
     public bool cannotShortTele;
 
+    // UI SECTION
+    public Slider meterSliderShort;
 
     void Start()
     {
         shortOnCooldown = false;
-    }
+        cooldownTotal = shortCooldown;
+}
     void Update()
     {
         ShortTeleport();
     }
     void ShortTeleport()
     {
-        if (Input.GetKey(m_short) && shortOnCooldown == false) // Activates Short-Ranged Teleportation
+        if (Input.GetKey(m_short) && shortOnCooldown == false) // Activates Short-Ranged Teleportation if not on cooldown
         {
-            if (cannotShortTele == false)
+            if (cannotShortTele == false) // Checks a bool set by the CheckAhead.cs script - looking for bad tele outcomes.
             {
-                Debug.Log("Short TP was triggered...");
                 Player.SetActive(false);
-                //playerPos.transform.position += playerPos.transform.forward * shortTeleport; // OLD Teleport
                 playerPos.transform.position = shortTeleTarget.position;
                 Player.SetActive(true);
-                Player.GetComponent<FPSMovement>().m_velocity.y = 0f;
-                shortOnCooldown = true;
-                baseCooldown = shortCooldown; // Cooldown reset
+                Player.GetComponent<FPSMovement>().m_velocity.y = 0f; // ensures player velocity vertically is reset.
+                shortOnCooldown = true; // Enters a 'cooldown' state.
+                baseCooldown = shortCooldown; // Cooldown time reset
             }
         }
         if (shortOnCooldown == true) // Cooldown calculations
         {
             baseCooldown -= Time.deltaTime;
+            float percentageResult = baseCooldown / cooldownTotal;
+            meterSliderShort.value = percentageResult;
             if (baseCooldown <= 0)
             {
                 shortOnCooldown = false;
